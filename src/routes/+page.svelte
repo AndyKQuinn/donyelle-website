12b43b3d4d2be6d45b77ae9a899202a00cefa8a1
Andy Quinn <andy.kettler.quinn@gmail.com>
2024-12-24 23:48:12 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { getUserContext } from '$lib/contexts/user'
+
+  const user = getUserContext()
+</script>
+
+{#if $user}
+  <p>Logged in as {$user.email}</p>
+{:else}
+  <p>Not logged in</p>
+{/if}
+
+<p>
+  Visit
+  <a href="https://github.com/jianyuan/pocketbase-sveltekit-auth">
+    our GitHub repository
+  </a>
+  to read the documentation.
+</p>
